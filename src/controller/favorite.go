d6bd88a2be5e29c1b90b47b0a21b51257a58579f
huangqiang <huangqiang97@126.com>
2022-06-05 20:14:18 +0800

    添加日志

--- src/controller/favorite.go
+++ src/controller/favorite.go
@@ -6,6 +6,7 @@ import (
 	"douyin-proj/src/service"
 	"douyin-proj/src/types"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
@@ -14,6 +15,7 @@ func FavoriteAction(c *gin.Context) {
 	// parse parameters
 	var favoriteRequest = types.FavoriteRequest{}
 	if err := c.ShouldBind(&favoriteRequest); err != nil {
+		log.Printf("反序列化点赞操作请求失败。err:%s\n", err)
 		c.JSON(http.StatusOK, types.FavoriteResponse{
 			Response: types.Response{StatusCode: ErrNo.ParamInvalid, StatusMsg: err.Error()},
 		})
@@ -22,6 +24,7 @@ func FavoriteAction(c *gin.Context) {
 	// check token
 	uId, err := util.VerifyToken(favoriteRequest.Token)
 	if err != nil {
+		log.Printf("登录失败，err:%s\n", err)
 		c.JSON(http.StatusOK, types.FavoriteResponse{
 			Response: ErrNo.AuthFailedResp,
 		})
@@ -36,6 +39,7 @@ func FavoriteAction(c *gin.Context) {
 			})
 			return
 		}
+		log.Printf("添加点赞记录成功。 uid:%d,videoId:%d\n", uId, favoriteRequest.VideoId)
 	case 2: // undo favorite
 		if err := service.UndoFavorite(uId, favoriteRequest.VideoId); err != nil {
 			c.JSON(http.StatusOK, types.FavoriteResponse{
@@ -43,6 +47,7 @@ func FavoriteAction(c *gin.Context) {
 			})
 			return
 		}
+		log.Printf("删除点赞记录成功。 uid:%d,videoId:%d\n", uId, favoriteRequest.VideoId)
 	}
 	c.JSON(http.StatusOK, types.FavoriteResponse{
 		Response: ErrNo.SuccessResp,
@@ -52,8 +57,10 @@ func FavoriteAction(c *gin.Context) {
 
 // FavoriteList all users have same favorite video list
 func FavoriteList(c *gin.Context) {
+	// parse parameters
 	var favoriteListRequest = types.FavoriteListRequest{}
 	if err := c.ShouldBind(&favoriteListRequest); err != nil {
+		log.Printf("反序列化获取点赞过视频操作请求失败。err:%s\n", err)
 		c.JSON(http.StatusOK, types.FavoriteListResponse{
 			Response: types.Response{StatusCode: ErrNo.ParamInvalid, StatusMsg: err.Error()},
 		})
@@ -63,14 +70,17 @@ func FavoriteList(c *gin.Context) {
 	// check token
 	uId, err := util.VerifyToken(favoriteListRequest.Token)
 	if err != nil {
+		log.Printf("登录失败，err:%s\n", err)
 		c.JSON(http.StatusOK, types.FavoriteListResponse{
 			Response: ErrNo.AuthFailedResp,
 		})
 		return
 	}
 
+	// 获得用户点赞过视频
 	videoList, err := service.GetFavoriteVideoListByUserId(favoriteListRequest.UserId, uId)
 	if err != nil {
+		log.Printf("获得用户点赞过视频失败。 uid:%d,err:%s\n", favoriteListRequest.UserId, err)
 		c.JSON(http.StatusOK, types.FavoriteListResponse{
 			Response: types.Response{StatusCode: ErrNo.UnknownError, StatusMsg: err.Error()},
 		})
@@ -81,4 +91,5 @@ func FavoriteList(c *gin.Context) {
 		Response:  ErrNo.SuccessResp,
 		VideoList: videoList,
 	})
+	log.Printf("获得用户点赞过视频成功。 uid:%d\n", favoriteListRequest.UserId)
 }
--- src/service/favorite_service.go
+++ src/service/favorite_service.go
@@ -3,23 +3,29 @@ package service
 import (
 	"douyin-proj/src/repository"
 	"douyin-proj/src/types"
+	"log"
 )
 
+// AddFavorite 添加点赞
 func AddFavorite(userId uint, videoId uint) error {
 	err := repository.InsertFavorite(&repository.Favorite{UserID: userId, VideoID: videoId})
 	return err
 }
 
+//UndoFavorite 取消点赞
 func UndoFavorite(userId uint, videoId uint) error {
 	err := repository.DeleteFavorite(&repository.Favorite{UserID: userId, VideoID: videoId})
 	return err
 }
 
+// GetFavoriteVideoListByUserId 根据qid获得用户点赞过视频,uId为当前登录用户
 func GetFavoriteVideoListByUserId(qId, uId uint) ([]types.Video, error) {
 	videos, err := repository.GetFavoriteVideoByUserId(qId)
 	if err != nil {
+		log.Printf("获取用户点赞过视频失败。uId:%d,err:%s\n", qId, err)
 		return nil, err
 	}
+	// 填充响应视频信息
 	var videoList = make([]types.Video, 0, len(videos))
 	for _, v := range videos {
 		video := types.Video{
@@ -32,10 +38,13 @@ func GetFavoriteVideoListByUserId(qId, uId uint) ([]types.Video, error) {
 		}
 		isFavorite := repository.IsFavorite(&repository.Favorite{UserID: uId, VideoID: v.ID})
 		video.IsFavorite = isFavorite
+		// 视频作者信息
 		u, err := repository.GetUserById(v.AuthorID)
 		if err != nil {
+			log.Printf("获取用户信息失败。uId:%d,err:%s\n", v.AuthorID, err)
 			return nil, err
 		}
+		// 当前用户与视频作者关注关系
 		isFollow := repository.GetRelation(&repository.Relation{UserID: uId, FollowID: v.AuthorID})
 		video.Author = types.User{
 			Id:            u.ID,
